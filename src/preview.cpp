5eb72b83115c508cb64db890407847118ee68d1a
Alan Johnson <alanjcd2004@gmail.com>
2025-04-30 21:03:48 -0500

    corrected scene files

--- src/preview.cpp
+++ src/preview.cpp
@@ -265,7 +265,7 @@ void mainLoop()
 
         runCuda();
 
-        string title = "CIS565 Path Tracer | " + utilityCore::convertIntToString(iteration) + " Iterations";
+        string title = "Path Tracer | " + utilityCore::convertIntToString(iteration) + " Iterations";
         glfwSetWindowTitle(window, title.c_str());
         glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
         glBindTexture(GL_TEXTURE_2D, displayImage);
